26df6c9a95a008ef786533f997b584b2d99e63b4
Robert Segal <rob@getsetgames.com>
2016-05-16 10:44:23 -0400

    Validation data function used for several tracking functions

--- Source/Upsight/Private/UpsightFunctions.cpp
+++ Source/Upsight/Private/UpsightFunctions.cpp
@@ -7,6 +7,25 @@
 
 #include "Android/AndroidJNI.h"
 #include "AndroidApplication.h"
+
+bool ValidateValues(TArray<FString> &Keys, TArray<FString> &Values)
+{
+    const int32 kNumKeys   = Keys.Num();
+    const int32 kNumValues = Values.Num();
+    
+    if (kNumKeys == 0 || kNumValues == 0)
+    {
+        return false;
+    }
+    
+    if (kNumKeys != kNumValues)
+    {
+        return false;
+    }
+    
+    return true;
+}
+
 #if PLATFORM_IOS
 
 #endif
